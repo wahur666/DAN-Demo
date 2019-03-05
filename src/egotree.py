a050b73093c81819cbef492c250aedfa6fed3198
wahur666 <imre.secodi@gmail.com>
2019-03-05 08:47:43 +0100

    Parent Tree Class created, values transformed from [0, 1] -> [0, 100]

--- src/egotree.py
+++ src/egotree.py
@@ -14,17 +14,30 @@ class Node:
         return self.label + " W: " + str(self.probability)
 
 
-class BinTree:
+class Tree:
 
     def __init__(self, root: Node):
         self.root = root
-        self.leaves: List[BinTree] = []
-
-    def weight(self):
-        return sum(n.root.probability for n in self.leaves) + self.root.probability
+        self.leaves: List = []
 
     def dept(self):
         return 1 + max(self.leaves, key=lambda x: x.dept()).dept() if self.leaves else 0
+
+    def __str__(self):
+        return self.root.label + " L: " + str(self.leaves)
+
+    def __repr__(self):
+        return self.root.label + " L: " + str(self.leaves)
+
+    def weight(self):
+        return sum(n.weight() for n in self.leaves) + self.root.probability
+
+
+class BinTree(Tree):
+
+    def __init__(self, root: Node):
+        super().__init__(root)
+        self.leaves: List[BinTree] = []
 
     def push(self, bintree):
         if len(self.leaves) != 2:
@@ -33,17 +46,11 @@ class BinTree:
             lightest_leave = min(self.leaves, key=lambda x: x.weight())
             lightest_leave.push(bintree)
 
-    def __str__(self):
-        return self.root.label + " L: " + str(self.leaves)
 
-    def __repr__(self):
-        return self.root.label + " L: " + str(self.leaves)
-
-
-class EgoTree:
+class EgoTree(Tree):
 
     def __init__(self, root: Node, delta: int):
-        self.root = root
+        super().__init__(root)
         self.delta = delta
         self.leaves: List[BinTree] = []
 
@@ -53,15 +60,6 @@ class EgoTree:
             lightest_leave.push(bintree)
         else:
             self.leaves.append(bintree)
-
-    def dept(self):
-        return 1 + max(self.leaves, key=lambda x: x.dept()).dept() if self.leaves else 0
-
-    def __str__(self):
-        return self.root.label + " L: " + str(self.leaves)
-
-    def __repr__(self):
-        return self.root.label + " L: " + str(self.leaves)
 
 
 def map_probabilities(p: List[Node]) -> Dict:
@@ -78,17 +76,17 @@ def create_egotree(source: Node, p: List[Node], delta: int):
 
 
 source = Node("T0", 0)
-p = [Node("T1", 0.24),
-     Node("T2", 0.2),
-     Node("T3", 0.1),
-     Node("T4", 0.1),
-     Node("T5", 0.1),
-     Node("T6", 0.1),
-     Node("T7", 0.07),
-     Node("T8", 0.05),
-     Node("T9", 0.02),
-     Node("T10", 0.01),
-     Node("T11", 0.01)]
+p = [Node("T1", 24),
+     Node("T2", 20),
+     Node("T3", 10),
+     Node("T4", 10),
+     Node("T5", 10),
+     Node("T6", 10),
+     Node("T7", 7),
+     Node("T8", 5),
+     Node("T9", 2),
+     Node("T10", 1),
+     Node("T11", 1)]
 n = 4
 
 egotree = create_egotree(source, p, n)
@@ -98,3 +96,4 @@ for item in egotree.leaves:
     print(item)
 
 print(egotree.dept())
+print(egotree.weight())
